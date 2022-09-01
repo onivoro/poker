7afa9fb7f8fd764dda35b34fd2cc510822d60022
icedlee337 <lee.e.norris@gmail.com>
2022-09-01 00:55:27 -0400

    chore: fnr onivoro => poker

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/onivoro'),
+      dir: require('path').join(__dirname, './coverage/poker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/environments/configs/firebase.config.ts
+++ src/environments/configs/firebase.config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
     apiKey: "AIzaSyCc7iCCry8QTlMCharSXs90BmaO6LCVYJs",
-    authDomain: "onivoro-com.firebaseapp.com",
-    projectId: "onivoro-com",
-    storageBucket: "onivoro-com.appspot.com",
+    authDomain: "poker-com.firebaseapp.com",
+    projectId: "poker-com",
+    storageBucket: "poker-com.appspot.com",
     messagingSenderId: "751736442899",
     appId: "1:751736442899:web:fd316a5353ca5ce242afe6"
   };
